e00b2dda2a7abd86fb172ce52fe67220c7cd2482
Jules Debbaut <jules.debbaut@student.kdg.be>
2024-12-05 14:35:34 +0100

    Basis aangelegd voor dim_client

---
+++ datawarehouse/student2/Python/S2_01_DIM_CLIENT.py
@@ -0,0 +1,99 @@
+import psycopg2
+
+db_config = {
+    'dbname': 'datawarehouse',
+    'user': 'postgres',
+    'password': '123456',
+    'host': 'localhost',
+    'port': '5432',
+}
+# Klantdata (voorbeeldrecords)
+# dit moet opgehaald worden van de users CSV
+customers = [
+    {
+        'CustomerID': 1,
+        'Address': 'Main Street 123',
+        'City': 'Cityville',
+        'PostalCode': '12345',
+        'SubscriptionType': 'Premium',
+        'ValidFrom': '2024-01-01',
+        'ValidTo': None,  # Huidige actieve rij
+        'IsActive': True
+    },
+    {
+        'CustomerID': 2,
+        'Address': 'Second Avenue 456',
+        'City': 'Townsville',
+        'PostalCode': '67890',
+        'SubscriptionType': 'Basic',
+        'ValidFrom': '2024-01-01',
+        'ValidTo': '2024-06-01',
+        'IsActive': False
+    },
+    {
+        'CustomerID': 2,
+        'Address': 'Third Street 789',
+        'City': 'Newtown',
+        'PostalCode': '67891',
+        'SubscriptionType': 'Premium',
+        'ValidFrom': '2024-06-02',
+        'ValidTo': None,
+        'IsActive': True
+    }
+]
+
+def connect_to_db(config):
+    try:
+        conn = psycopg2.connect(**config)
+        print("Connection to database established")
+        return conn
+    except Exception as e:
+        print(f"Fout bij verbinden met database: {e}")
+
+def fill_table_dim_client(cursor_dwh, table_name='dim_client'):
+    insert_query = f"""
+    INSERT INTO {table_name} (
+     CustomerID, Address, City, PostalCode, SubscriptionType, ValidFrom, ValidTo, IsActive
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    """
+
+    for customer in customers:
+        try:
+            cursor_dwh.execute(insert_query, (
+                customer['CustomerID'],
+                customer['Address'],
+                customer['City'],
+                customer['PostalCode'],
+                customer['SubscriptionType'],
+                customer['ValidFrom'],
+                customer['ValidTo'],
+                customer['IsActive']
+            ))
+            print(f"Klant {customer['CustomerID']} is inserted successfully")
+        except Exception as e:
+            print(f"Error adding client: {e}")
+
+
+def main():
+    conn = connect_to_db(db_config)
+    if conn is None:
+        return
+
+    try:
+        cursor_dwh = conn.cursor()
+
+        fill_table_dim_client(cursor_dwh)
+
+        conn.commit()
+        print("All changes saved successfully")
+
+    except Exception as e:
+        print(f"Error during operating: {e}")
+        conn.rollback()
+    finally:
+        if conn:
+            cursor_dwh.close()
+            conn.close()
+            print("Closed connection to database.")
+
+main()
