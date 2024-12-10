09a9e578b6d7710f29033472a5ea8767baf120cc
Jules Debbaut <jules.debbaut@student.kdg.be>
2024-12-10 19:39:06 +0100

    Nieuwe CREATE voor FACT_RIDE toegevoegd

--- nosql/student2/SQL/dwh_CREATE.sql
+++ nosql/student2/SQL/dwh_CREATE.sql
@@ -24,3 +24,20 @@ CREATE TABLE dim_lock (
                           stationType VARCHAR(20),            -- Type station (bijv. 'Standard', 'Large')
                           isStep BOOLEAN DEFAULT FALSE        -- TRUE voor ritten zonder slot
 );
+
+--andere dimensies moeten nog aangemaakt worden om dit in orde te krijgen!
+CREATE TABLE FACT_RIDE (
+                           RIDE_SK SERIAL PRIMARY KEY,       -- Surrogaatsleutel
+                           DATE_SK INTEGER NOT NULL,         -- Verwijzing naar DIM_DATE
+                           WEATHER_SK INTEGER NOT NULL,      -- Verwijzing naar DIM_WEATHER
+                           CUSTOMER_SK INTEGER NOT NULL,     -- Verwijzing naar DIM_CUSTOMER
+                           START_LOCK_SK INTEGER NOT NULL,   -- Verwijzing naar DIM_LOCK (startslot)
+                           END_LOCK_SK INTEGER NOT NULL,     -- Verwijzing naar DIM_LOCK (eindslot)
+                           Duration INTERVAL NOT NULL,       -- Duur van de rit
+                           Distance DECIMAL(10,2),           -- Afstand van de rit in kilometer
+                           FOREIGN KEY (DATE_SK) REFERENCES DIM_DATE(DATE_SK),
+                           FOREIGN KEY (WEATHER_SK) REFERENCES DIM_WEATHER(WEATHER_SK),
+                           FOREIGN KEY (CUSTOMER_SK) REFERENCES DIM_CUSTOMER(CUSTOMER_SK),
+                           FOREIGN KEY (START_LOCK_SK) REFERENCES DIM_LOCK(LOCK_SK),
+                           FOREIGN KEY (END_LOCK_SK) REFERENCES DIM_LOCK(LOCK_SK)
+);
